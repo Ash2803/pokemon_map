47a7c426a01a2ade7b6e1b135cd4f1c134f54ec9
Ash2803 <ashot.93@mail.ru>
2022-12-01 17:52:04 +0400

    Deleted redundant related_name argument in PokemonEntit model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -30,7 +30,6 @@ class PokemonEntity(models.Model):
     stamina = models.IntegerField(null=True, verbose_name="Выносливость")
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE,
                                 null=True,
-                                related_name='pokemons',
                                 verbose_name="Покемон")
 
     def __str__(self):
